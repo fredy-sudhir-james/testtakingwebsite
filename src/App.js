a990cc743dcd06e8165d4e6042540f2de748a880
Fredy Sudhir <39754501+fredysj@users.noreply.github.com>
2022-10-08 21:04:20 -0400

    Landing page basic text and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { grey, teal } from '@mui/material/colors';
+import { grey, teal, cyan } from '@mui/material/colors';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Home from './Home';
 import Test from './Test';
@@ -13,9 +14,11 @@ import './App.css';
 
 const theme = createTheme({
 	palette: {
-		colors: {
-			white: '#fff',
+		primary: {
 			main: teal[400],
+		},
+		secondary: {
+			main: cyan[700],
 		}
 	}
 });
@@ -27,18 +30,18 @@ function App() {
 			<Container maxWidth="lg" sx={{py: 3}}>
 				<Grid container direction="row" justifyContent="space-between" alignItems="center">
 					<Link to="/" className='app-logo' style={{ textDecoration: 'none', color: teal[400] }}><h1 style={{ "margin": 0 }}>TTW</h1></Link>
-					<Button variant="contained" size="large" endIcon={<ArrowForwardIcon />}>Next</Button>
+					<Button variant="contained" size="large" endIcon={<ArrowForwardIcon />} color="primary">Next</Button>
 				</Grid>
 			</Container>
 		</header>
-		<div className="app-body" style={{ backgroundColor: grey[200] }} >
-			<Container maxWidth="lg" sx={{ px: { xs: 5, md: 8}, py: {xs: 4, md: 6}, bgcolor: 'colors.white', borderRadius: 2 }}>
+		<Box sx={{ py: 10, bgcolor: grey[200] }}>
+			<Container maxWidth="lg" sx={{ px: { xs: 5, md: 8}, py: {xs: 4, md: 8}, bgcolor: 'white', borderRadius: 2 }}>
 				<Routes>
 					<Route path="/" element={<Home />}/>
 					<Route path="/test" element={<Test />}/>
 				</Routes>
 			</Container>
-		</div>
+		</Box>
 	</ThemeProvider>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,43 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { indigo, grey, teal, cyan } from '@mui/material/colors';
 
 function Home() {
 	return (
 		<Fragment>
-			<Container maxWidth="sm" sx={{ textAlign: "center" }}>
-				<h2>Landing page title</h2>
+			<Container maxWidth="md" sx={{ textAlign: "center" }}>
+				<Stack spacing={4}>
+					<Typography variant="h4" component="h1">Landing page</Typography>
+					<p style={{marginBottom: 0, fontSize: 22}}>This is the test taking website for Technical assesment on Frontend Developer role</p>
+				</Stack>
+				
 			</Container>
+
+			<Grid container justifyContent="center" spacing={0} sx={{ gap: 5, mt: 8}} className="app-body__cards">
+				<Grid item sx={{ maxWidth: 350, bgcolor: 'grey.100', borderRadius: 1.5}} className="app-body__card" style={{ overflow: "hidden"}}>
+					<Typography variant="h2" className="app-body__card__title" sx={{ bgcolor: indigo[900], py: 2, px: 4, fontSize: 20, color: 'white' }}>English</Typography>
+					<Stack className='app-body__card__body' sx={{px: 4, pt: 3, pb: 5}} spacing={4}>
+						<p style={{margin: 0, fontSize: 18}}>This test contains 10 dummy question related to English language. Click the details button to start the test.</p>
+						<Link to="/test" style={{display: 'inline-block', textDecoration: 'none'}}>
+							<Button variant="outlined" size="large" sx={{ borderColor: grey[500], color: grey[500], ":hover": {borderColor: cyan[700], color: cyan[700] } }} id="english">Details</Button>
+						</Link>
+					</Stack>
+				</Grid>
+				<Grid item sx={{ maxWidth: 350, bgcolor: 'grey.100', borderRadius: 1.5}} className="app-body__card" style={{ overflow: "hidden"}}>
+					<Typography variant="h2" className="app-body__card__title" sx={{ bgcolor: indigo[900], py: 2, px: 4, fontSize: 20, color: 'white' }}>Mathematics</Typography>
+					<Stack className='app-body__card__body' sx={{px: 4, pt: 3, pb: 5}} spacing={4}>
+						<p style={{margin: 0, fontSize: 18}}>This test contains 10 dummy question related to Maths. Click the details button to start the test.</p>
+						<Link to="/test" style={{display: 'inline-block', textDecoration: 'none'}}>
+							<Button variant="outlined" size="large" sx={{ borderColor: grey[500], color: grey[500], ":hover": {borderColor: cyan[700], color: cyan[700] } }} id="math">Details</Button>
+						</Link>
+					</Stack>
+				</Grid>
+			</Grid>
 		</Fragment>
 	)
 }
